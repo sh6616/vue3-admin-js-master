26b7f0ae2343af71b9a9cb9e813f91c1a8d11c05
sh6616 <15539550863@163.com>
2022-08-28 10:28:34 +0800

    1

---
+++ src/views/acount/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="account">
+    <div class="form-wrap">
+      <a-form name="custom-validation" ref="formRef" v-bind="formConfig.layout">
+        <a-form-item>
+          <label>用户名</label>
+          <a-input type="password" autocomplete="off" />
+        </a-form-item>
+        <a-form-item>
+          <label>密码</label>
+          <a-input type="password" autocomplete="off" />
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" html-type="submit" block>Submit</a-button>
+        </a-form-item>
+      </a-form>
+      <div class="text-center fs-12">
+        <a href="" class="color-white">忘记密码</a> |
+        <a href="" class="color-white">注册</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { reactive } from "vue";
+export default {
+  name: "Login",
+  setup(props) {
+    const formConfig = reactive({
+      layout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 24 },
+      },
+    });
+
+    return {
+      formConfig,
+    };
+  },
+};
+</script>
+<style lang="scss">
+@import "./style.scss";
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  //构建项目生成的目录
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devDist',
+  //默认情况下 babel-loader 会忽略所有 node_modules 中的文件。你可以启用本选项，以避免构建后的代码中出现未转译的第三方依赖。
   transpileDependencies: true,
+  //关闭语法的自动检测
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/main.scss";`
+      }
+    }
+  },
+
 });
